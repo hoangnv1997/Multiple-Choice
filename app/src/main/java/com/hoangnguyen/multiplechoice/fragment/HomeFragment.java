5355f98a5e857251fbf2bb7adf217eb808993be5
Hoang Nguyen <nguyenvanhoangcfc@gmail.com>
2020-09-06 10:29:30 +0700

    commit

--- app/src/main/java/com/hoangnguyen/multiplechoice/fragment/HomeFragment.java
+++ app/src/main/java/com/hoangnguyen/multiplechoice/fragment/HomeFragment.java
@@ -32,7 +32,7 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
-import java.io.BufferedInputStream;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
